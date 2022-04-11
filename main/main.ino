bf46a590ad833556121e887bc15abd204ac0e249
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-04-12 04:28:04 +0900

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,8 @@
+//#define DEBUG
+
 #define COLs 32  //行当たり最大文字数:
 
-const char BootMenu[] = {"BASIC Emulator on Arduino Ver-0.1.1-beta"};
+const char BootMenu[] = {"BASIC Emulator on Arduino Ver-0.1.2"};
 
 uint8_t workingCOL = 0;
 
@@ -27,8 +29,12 @@ void loop() {
 
 void ReturnTXT(uint8_t datSize) {
   for (uint8_t i = 0; i < datSize; i++) {
+#ifdef DEBUG
     Serial.print(Buffer[i], HEX);
     Serial.print(" ");
+#else
+    Serial.write(Buffer[i]);
+#endif
   }
 }
 
@@ -37,21 +43,36 @@ void runFunc() {
   if (Buffer[0] == 0x4C && Buffer[1] == 0x45 && Buffer[2] == 0x44 && Buffer[3] == 0x20 && Buffer[5] == 0x0A) {
     if (Buffer[4] == 0x30) {
       digitalWrite(13,  LOW);
+      Serial.println(">> Done");
     } else if(Buffer[4] == 0x31){
       digitalWrite(13, HIGH);
+      Serial.println(">> Done");
     } else {
-      Serial.println("Syntax Error...");
+      Serial.println(">> Syntax Error...");
     }
   }
   //LED関数ここまで:
 
-  //WAIT関数ここから:
-  if (Buffer[0] == 0x57 && Buffer[1] == 0x41 && Buffer[2] == 0x49 && Buffer[3] == 0x54 && Buffer[4] == 0x20){
-    //作業中 in here:
+  //PRINT関数ここから:  
+  else if ((Buffer[0] == 0x50 && Buffer[1] == 0x52 && Buffer[2] == 0x49 && Buffer[3] == 0x4E && Buffer[4] == 0x54 && Buffer[5] == 0x20) || (Buffer[0] == 0x3F && Buffer[1] == 0x20)){
+    uint8_t i;
+    if (Buffer[0] == 0x50){
+      i = 6;
+    } else {
+      i = 2;
+    }
+    Serial.print(">> ");
+    while(Buffer[i] != 0x0A){
+      Serial.write(Buffer[i]);
+      i++;
+    }
+    Serial.println();
   }
+  //PRINT関数ここまで:
   else {
-    Serial.println("Syntax Error...");
+    Serial.println(">> Syntax Error...");
   }
+  
   //↓のやつ本当ならセーブ後が望ましい:
   for(uint8_t i = 0; i < COLs; i++){
     Buffer[i] = 0;
