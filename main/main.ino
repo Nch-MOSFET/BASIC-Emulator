abab1dde8aa464e3d3f2198ca5afb67cc160d054
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-04-16 10:55:00 +0900

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,16 @@
 //#define DEBUG
 
 #define COLs 32  //行当たり最大文字数:
+#define ROWs 32  //最大行数:
 
 const char BootMenu[] = {"BASIC Emulator on Arduino Ver-0.1.2-beta"};
 
-uint8_t workingCOL = 0;
-
+uint8_t BASIC[COLs + 1][ROWs];
 uint8_t Buffer[COLs];
+
+uint8_t workingCOL = 0;
+uint8_t workingROW = 0;
+uint8_t ROWsValue  = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -39,6 +43,21 @@ void ReturnTXT(uint8_t datSize) {
 }
 
 void runFunc() {
+  //行番号ここから:
+  if (Buffer[0] > 0x2F && 0x3F > Buffer[0]){
+    while (Buffer[0] >= 0x30 && 0x39 >= Buffer[0]) {
+      ROWsValue = ROWsValue * 10;
+      ROWsValue = Buffer[0];
+    }
+#ifdef DEBUG
+    Serial.print("ROWsValue = ");
+    Serial.print(ROWsValue);
+#endif
+    BASIC[33][workingROW] = ROWsValue;
+    ROWsValue = 0;
+    workingCOL++;
+  }
+  //行番号ここまで:
   //LED関数ここから:
   if (Buffer[0] == 0x4C && Buffer[1] == 0x45 && Buffer[2] == 0x44 && Buffer[3] == 0x20 && Buffer[5] == 0x0A) {
     if (Buffer[4] == 0x30) {
@@ -98,4 +117,10 @@ void readSerials() {
   ReturnTXT(workingCOL);
   //Serial.println(workingCOL);
   runFunc();
+}
+
+void BufferShift(uint8_t val) {
+  for(uint8_t i = COLs; i > val; i--) {
+    Buffer[i - val] = Buffer[i];
+  }
 }
