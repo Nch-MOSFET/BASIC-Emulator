bb6d13ce8f802bc028bd7255fc45706aed66a80b
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-07-01 15:44:43 +0900

    Create whole_char_detect.ino

---
+++ appendix/whole_char_detect/whole_char_detect.ino
@@ -0,0 +1,28 @@
+#define BaudRate 115200
+
+void setup() {
+  Serial.begin(BaudRate);
+
+  Serial.println("number, data, isAlpha, isAlphaNumeric, isAscii, isControl, isDigit, isGraph, isHexadecimalDigit, isLowerCase, isUpperCase, isPrintable, isPunct, isSpace, isWhitespace");
+
+  for(uint8_t i = 0; i < 256; i++) {
+    Serial.print(i);                       Serial.print(", ");
+    Serial.write(i);                       Serial.print(", ");
+    Serial.print(isAlpha(i));              Serial.print(", ");
+    Serial.print(isAlphaNumeric(i));       Serial.print(", ");
+    Serial.print(isAscii(i));              Serial.print(", ");
+    Serial.print(isControl(i));            Serial.print(", ");
+    Serial.print(isDigit(i));              Serial.print(", ");
+    Serial.print(isGraph(i));              Serial.print(", ");
+    Serial.print(isHexadecimalDigit(i));   Serial.print(", ");
+    Serial.print(isLowerCase(i));          Serial.print(", ");
+    Serial.print(isUpperCase(i));          Serial.print(", ");
+    Serial.print(isPrintable(i));          Serial.print(", ");
+    Serial.print(isPunct(i));              Serial.print(", ");
+    Serial.print(isSpace(i));              Serial.print(", ");
+    Serial.print(isWhitespace(i));         Serial.println();
+  }
+}
+
+void loop() {
+}
