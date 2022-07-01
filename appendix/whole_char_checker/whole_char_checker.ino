9cf2f4cb23a7780ba80321ac82c8e7d5a1ae0317
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-07-01 22:31:23 +0900

    Update whole_char_checker.ino

--- appendix/whole_char_checker/whole_char_checker.ino
+++ appendix/whole_char_checker/whole_char_checker.ino
@@ -3,23 +3,25 @@
 void setup() {
   Serial.begin(BaudRate);
 
-  Serial.println("number, data, isAlpha, isAlphaNumeric, isAscii, isControl, isDigit, isGraph, isHexadecimalDigit, isLowerCase, isUpperCase, isPrintable, isPunct, isSpace, isWhitespace");
+  Serial.println("Start...");
 
-  for(uint8_t i = 0; i < 256; i++) {
-    Serial.print(i);                       Serial.print(", ");
-    Serial.write(i);                       Serial.print(", ");
-    Serial.print(isAlpha(i));              Serial.print(", ");
-    Serial.print(isAlphaNumeric(i));       Serial.print(", ");
-    Serial.print(isAscii(i));              Serial.print(", ");
-    Serial.print(isControl(i));            Serial.print(", ");
-    Serial.print(isDigit(i));              Serial.print(", ");
-    Serial.print(isGraph(i));              Serial.print(", ");
-    Serial.print(isHexadecimalDigit(i));   Serial.print(", ");
-    Serial.print(isLowerCase(i));          Serial.print(", ");
-    Serial.print(isUpperCase(i));          Serial.print(", ");
-    Serial.print(isPrintable(i));          Serial.print(", ");
-    Serial.print(isPunct(i));              Serial.print(", ");
-    Serial.print(isSpace(i));              Serial.print(", ");
+  Serial.println("number,data,isAlpha,isAlphaNumeric,isAscii,isControl,isDigit,isGraph,isHexadecimalDigit,isLowerCase,isUpperCase,isPrintable,isPunct,isSpace,isWhitespace");
+
+  for (uint16_t i = 0; i < 256; i++) {
+    Serial.print(i);                       Serial.print(",");
+    Serial.write(i);                       Serial.print(",");
+    Serial.print(isAlpha(i));              Serial.print(",");
+    Serial.print(isAlphaNumeric(i));       Serial.print(",");
+    Serial.print(isAscii(i));              Serial.print(",");
+    Serial.print(isControl(i));            Serial.print(",");
+    Serial.print(isDigit(i));              Serial.print(",");
+    Serial.print(isGraph(i));              Serial.print(",");
+    Serial.print(isHexadecimalDigit(i));   Serial.print(",");
+    Serial.print(isLowerCase(i));          Serial.print(",");
+    Serial.print(isUpperCase(i));          Serial.print(",");
+    Serial.print(isPrintable(i));          Serial.print(",");
+    Serial.print(isPunct(i));              Serial.print(",");
+    Serial.print(isSpace(i));              Serial.print(",");
     Serial.print(isWhitespace(i));         Serial.println();
   }
 }
