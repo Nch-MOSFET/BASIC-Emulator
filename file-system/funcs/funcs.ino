f8fee1558fa3756f3dffe6f20e582664250793eb
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-07-06 15:26:45 +0900

    Update funcs.ino

--- file-system/funcs/funcs.ino
+++ file-system/funcs/funcs.ino
@@ -1,11 +1,13 @@
+#include "./textsDefine.h"
+
 uint8_t dataExchanger(uint8_t dat) {
-       if(47 < dat || dat < 58 ) {  // 0 ~ 9:
+       if(48 <= dat || dat <= 57 ) {  // 0 ~ 9:
     return dat - 48;
   }
-  else if(64 < dat || dat < 91 ) {  // A ~ Z:
+  else if(65 <= dat || dat <= 90 ) {  // A ~ Z:
     return dat - 55;
   }
-  else if(96 < dat || dat < 123) {  // a ~ z:
+  else if(97 <= dat || dat <= 122) {  // a ~ z:
     return dat - 87;
   }
   // 文字以外の記号に関するコードをこの辺に書く:
