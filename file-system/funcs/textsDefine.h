9670e0e1f25e7a18a8297ed44f4ef770b8153ace
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-07-06 15:23:36 +0900

    Create textsDefine.h

---
+++ file-system/funcs/textsDefine.h
@@ -0,0 +1,19 @@
+#define _A 10
+#define _B 11
+#define _C 12
+#define _D 13
+
+//中略:
+
+#define _Space 36
+
+#define _! 37
+#define _" 38
+#define _# 39
+#define _$ 40
+
+//中略:
+
+/*
+ * https://github.com/Nch-MOSFET/BASIC-Emulator/tree/dev-FileFormat/file-system/README.md
+ */
