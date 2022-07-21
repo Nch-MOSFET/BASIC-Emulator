456ea9ee71a490d48d1e0b5d00a1eabf0996ce00
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-07-21 20:04:51 +0900

    Update textsDefine.h

--- file-system/funcs/textsDefine.h
+++ file-system/funcs/textsDefine.h
@@ -1,57 +1,67 @@
-#define _A            10
-#define _B            11
-#define _C            12
-#define _D            13
-#define _E            14
-#define _F            15
-#define _G            16
-#define _H            17
-#define _I            18
-#define _J            19
-#define _K            20
-#define _L            21
-#define _M            22
-#define _N            23
-#define _O            24
-#define _P            25
-#define _Q            26
-#define _R            27
-#define _S            28
-#define _T            29
-#define _U            30
-#define _V            31
-#define _W            32
-#define _X            33
-#define _Y            34
-#define _Z            35
+#define _Blank        0
 
-#define _Space        36
+#define _0            1
+#define _1            2
+#define _2            3
+#define _3            4
+#define _4            5
+#define _5            6
+#define _6            7
+#define _7            8
+#define _8            9
+#define _9            10
 
-#define _Exclamation  37  // !
-#define _DoubleQuotes 38  // "
-#define _Sharp        39  // #
-#define _Dollars      40  // $
-#define _Percent      41  // %
-#define _Ampersand    42  // &
-#define _Bar          43  // |
-#define _ParenthesesF 44  // (
-#define _ParenthesesB 45  // )
-#define _Asterisk     46  // *
-#define _Plus         47  // +
-#define _Comma        48  // ,
-#define _Hyphen       49  // -
-#define _Period       50  // .
-#define _Slash        51  // /
-#define _colon        52  // :
-#define _SemiColon    53  // ;
-#define _LessThan     54  // <
-#define _Equal        55  // =
-#define _GreaterThan  56  // >
-#define _Question     57  // ?
-#define _Caret        58  // ^
-#define _UnderBar     59  // _
-#define _Apostrophe   60  // '
-#define _Tilde        61  // ~
+#define _A            11
+#define _B            12
+#define _C            13
+#define _D            14
+#define _E            15
+#define _F            16
+#define _G            17
+#define _H            18
+#define _I            19
+#define _J            20
+#define _K            21
+#define _L            22
+#define _M            23
+#define _N            24
+#define _O            25
+#define _P            26
+#define _Q            27
+#define _R            28
+#define _S            29
+#define _T            30
+#define _U            31
+#define _V            32
+#define _W            33
+#define _X            34
+#define _Y            35
+#define _Z            36
+#define _Space        37
+#define _Exclamation  38  // !
+#define _DoubleQuotes 39  // "
+#define _Sharp        40  // #
+#define _Dollars      41  // $
+#define _Percent      42  // %
+#define _Ampersand    43  // &
+#define _Bar          44  // |
+#define _ParenthesesF 45  // (
+#define _ParenthesesB 46  // )
+#define _Asterisk     47  // *
+#define _Plus         48  // +
+#define _Comma        49  // ,
+#define _Hyphen       50  // -
+#define _Period       51  // .
+#define _Slash        52  // /
+#define _colon        53  // :
+#define _SemiColon    54  // ;
+#define _LessThan     55  // <
+#define _Equal        56  // =
+#define _GreaterThan  57  // >
+#define _Question     58  // ?
+#define _Caret        59  // ^
+#define _UnderBar     60  // _
+#define _Apostrophe   61  // '
+#define _Tilde        62  // ~
 
-#define _Blank
 #define _Enter        63
